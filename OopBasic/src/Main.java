d3b2c1f76956034116aee71caa814fa628497a25
emilio <emilioxrf@outlook.com>
2024-03-05 15:27:47 +0800

    2024!

--- OopBasic/src/Main.java
+++ OopBasic/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args){
-        var ming = new Person();
+        Person ming = new Person();
         ming.setName("Xiao Ming"); // 设置name
         ming.setAge(12); // 设置age
         System.out.println(ming.getName() + ", " + ming.getAge());
