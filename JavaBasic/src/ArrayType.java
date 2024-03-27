f443577e280f7bb141d4d33f2405738831631837
emilio <emilioxrf@outlook.com>
2024-03-27 18:02:52 +0800

    JavaBasic finished

--- JavaBasic/src/ArrayType.java
+++ JavaBasic/src/ArrayType.java
@@ -1,6 +1,6 @@
 public class ArrayType {
     public static void main(String[] args){
-//        Array1 a = new Array1();
+        Array1 a = new Array1();
         StringArray1 sa1 = new StringArray1();
     }
 }
@@ -9,11 +9,18 @@ class Array1{
     Array1(){
         int[] ns={68, 79, 91, 85, 62};
         ns[0]=33;
-//        System.out.println(ns[0]);
 //        System.out.println(ns[4]);
         for(int i=0;i<5;i++){
             System.out.println(ns[i]);
         }
+        int[] ns0= new int[5];
+        ns0[0] = 68;
+        ns0[1] = 79;
+        ns0[2] = 91;
+        ns0[3] = 85;
+        ns0[4] = 62;
+        System.out.println();
+        System.out.println(ns0.length);
 
     }
 }
--- JavaBasic/src/CharAndString.java
+++ JavaBasic/src/CharAndString.java
@@ -8,34 +8,49 @@ public class CharAndString {
 
 class Chars{
     public Chars(){
+//        一个char保存一个Unicode字符
         char c1 = 'A';
         int c2 = '中';
+        System.out.println(c2);
         System.out.println((char)c2);
     }
 }
 
 class Strings{
     public Strings(){
+        /**
+         * 常见的转义字符包括：
+         *     \" 表示字符"
+         *     \' 表示字符'
+         *     \\ 表示字符\
+         *     \n 表示换行符
+         *     \r 表示回车符
+         *     \t 表示Tab
+         */
+        //一个字符串可以存储0个到任意个字符
         String s1="Fuck you!";
         String s2 = "ABC and \u4e2d\u6587";
         int i1=5;
         float f1=4.44f;
-//        System.out.println(s1+s2+i1+f1);
-//        System.out.println(i1+f1+s1+f1);
+        System.out.println(s1+s2+i1+f1);
+        //要完整拼接，第一个必须是字符串（使用“”+）
+        System.out.println(""+i1+f1+s1+f1);
+        System.out.println(i1+f1+s1+f1);
         String s3=
-                "SELECT * FROM" +
-                "users" +
-                "WHERE id > 100" +
+                "SELECT * FROM\n" +
+                "users\n" +
+                "WHERE id > 100\n" +
                 "ORDER BY name DESC";
         System.out.println(s3);
         System.out.println("finished");
-
+        test();
     }
     static void test(){
         String s = "hello";
         String t = s;
         s = "world";
         System.out.println(t); // t是"hello"还是"world"?
+        //Java的字符串除了是一个引用类型外，还有个重要特点，就是字符串不可变
     }
 }
 
@@ -46,7 +61,6 @@ class Practice{
         int b = 105;
         int c = 65281;
 
-        // FIXME:
         String s = ""+(char)a+(char)b+(char)c;
         System.out.println(s);
     }
